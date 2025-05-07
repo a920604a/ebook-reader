ebc2b3e6e659a70d37a4c34327eebf941857ff69
a920604a <a920604a@users.noreply.github.com>
2025-05-07 07:27:02 +0000

    deploy: 9580094b578bf48fe89fa96e18aaa63adf9a6a3f

---
+++ static/js/952.79cc2fa9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkebook_reader=self.webpackChunkebook_reader||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.79cc2fa9.chunk.js.map
